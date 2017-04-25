3c694c73c9a5d80a506d786bb344cb8c1c130db7
kevsalazararaya <kevin.salazar@ucrso.info>
2017-04-25 12:20:23 -0600

    jajaj

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - JK Bastar
                 </div>
 
                 <div class="links">
