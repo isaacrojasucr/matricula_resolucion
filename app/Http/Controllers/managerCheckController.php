3507ab60235334f7ac8e6eedca5548a255521f67
kevsalazararaya <kevin.salazar@ucrso.info>
2017-06-14 22:11:44 -0600

    Revisión Coordinador 40%
    Falta que tome el coordinador de la sesión y enviar el correo.

--- app/Http/Controllers/managerCheckController.php
+++ app/Http/Controllers/managerCheckController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\pwcnm_registration_process;
+use App\pwcnm_inscriptionRequest;
+use App\course;
 
 class managerCheckController extends Controller
 {
@@ -13,6 +16,28 @@ class managerCheckController extends Controller
      */
     public function index(Request $request)
     {
-        return view('inscription.manager.managerCheck');
+        $process = pwcnm_registration_process::all();
+        $process = $process->last();
+        $process = $process->id;
+
+        $requested = pwcnm_inscriptionRequest::where('fk_process', '=', $process)->get();
+
+        $petitions = array();
+        $i = 0;
+        foreach ($requested as $item) {
+            if ($item->fk_career == '1') {
+                $petitions = array_add($petitions,$i,$item);
+                $i++;
+            }
+        }
+
+        $courses = array();
+
+        for ($p = 0; $p < count($petitions); $p++) {
+            $temp = course::findOrFail($petitions[$p]->fk_course);
+            $courses = array_add($courses, $p, $temp);
+        }
+
+        return view('inscription.manager.managerCheck', compact('petitions', 'courses'));
     }
 }
--- resources/views/inscription/manager/managerCheck.blade.php
+++ resources/views/inscription/manager/managerCheck.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
 
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">@lang('form.studentRequestCheck')</div>
 
@@ -20,10 +20,28 @@
                                 <th>@lang('form.carne')</th>
                                 <th>@lang('form.courseName')</th>
                                 <th>@lang('form.timesAttended')</th>
+                                <th>@lang('form.phone')</th>
+                                <th>@lang('form.observation')</th>
+                                <th>@lang('form.act')</th>
                             </tr>
                             </thead>
                             <tbody>
+                            @for($i = 0; $i < count($petitions); $i++)
 
+                                <tr>
+                                    <td>{{ $petitions[$i]->studentName }}</td>
+                                    <td>{{ $petitions[$i]->studentId }}</td>
+                                    <td>{{ $courses[$i]->name}}</td>
+                                    <td>{{ $petitions[$i]->timesAttended}}</td>
+                                    <td>{{ $petitions[$i]->phone }}</td>
+                                    <td>{{ $petitions[$i]->observations}}</td>
+
+                                    <td>
+                                        <a title= @lang('form.accept') ><button class="btn btn-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button></a>
+                                        <a href="{{ url('/asd/ads' . $petitions[$i]->email) }}" title= @lang('form.deny') ><button class="btn btn-danger"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></a>
+                                    </td>
+                                </tr>
+                            @endfor
                             </tbody>
                         </table>
                      </div>
