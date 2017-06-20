f4f4d6b20b831c7ad85109235ed491fc6342c8f1
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2017-06-19 22:42:02 -0600

    admin

---
+++ resources/views/inscription/admin/allPetitions.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="col-md-8">
+        <div class="panel panel-info">
+
+            <div class="panel-heading">
+                @lang('form.actualPetitions');
+            </div>
+
+            <div class="panel-body">
+
+            </div>
+
+        </div>
+    </div>
+
+@endsection  
--- resources/views/inscription/admin/asd.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Isaac
- * Date: 14/6/2017
- * Time: 7:56 PM
- */
