18df24d16735373d09681b7b0735423a8e78bad4
kevsalazararaya <kevin.salazar@ucrso.info>
2017-06-28 20:37:24 -0600

    Inicio home 5%

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container" id="content">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+    <div class="container" id="content">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="panel panel-primary center">
+                    <div class="panel-heading">@lang('form.processInfo')</div>
+                    <div class="panel-body" style="text-align: left;">
+                        <div class="col-md-12">
+                            <div class="col-md-6">
+                                <h4>@lang('form.actualProcessState')</h4>
+                                <hr style="width:100%;">
+                                <h4>@lang('form.initialDa')</h4>
+                                <hr style="width:100%;">
+                                <h4>@lang('form.actualProcessState')</h4>
+                                <hr style="width:100%;">
+                                <h4>@lang('form.actualProcessState')</h4>
+                            </div>
+                            <div class="col-md-6">
+                                <h4></h4>
+                                <hr style="width:100%;">
+                                <h4></h4>
+                                <hr style="width:100%;">
+                                <h4></h4>
+                                <hr style="width:100%;">
+                                <h4></h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="panel panel-primary center">
+                    <div class="panel-heading">@lang('form.managerSum')</div>
+                    <div class="panel-body">
 
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-</div>
 @endsection
