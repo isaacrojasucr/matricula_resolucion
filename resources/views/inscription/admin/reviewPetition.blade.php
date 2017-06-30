79459c2e79b10b00678a785ad0d136ffede9fcd9
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2017-06-29 21:04:25 -0600

    old petitions fixed

--- resources/views/inscription/admin/reviewPetition.blade.php
+++ resources/views/inscription/admin/reviewPetition.blade.php
@@ -10,50 +10,56 @@
                         @lang('form.oldPetitions')
                     </div>
                     <div class="panel-body">
-                        <div class="table-responsive">
-                            <table class="table table-borderless">
-                                <thead>
+
+                        @if(count($data)>0)
+                            <div class="table-responsive">
+                                <table class="table table-borderless">
+                                    <thead>
                                     <tr style="font-weight: bold">
-                                        <td >@lang('form.year')</td>
-                                        <td >@lang('form.period')</td>
-                                        <td >@lang('form.course')</td>
-                                        <td >@lang('form.location')</td>
-                                        <td >@lang('form.state')</td>
-                                        <td >@lang('form.comments')</td>
+                                        <td>@lang('form.year')</td>
+                                        <td>@lang('form.period')</td>
+                                        <td>@lang('form.course')</td>
+                                        <td>@lang('form.location')</td>
+                                        <td>@lang('form.state')</td>
+                                        <td>@lang('form.comments')</td>
                                     </tr>
-                                </thead>
+                                    </thead>
 
-                                <tbody>
-                                @foreach($data as $item)
-                                    <tr>
-                                        <td>{{$item->anno}}</td>
-                                        <td>{{$item->period}}</td>
-                                        <td>{{$item->course}}</td>
-                                        <td>{{$item->location}}</td>
-                                        <td>
-                                            @if($item->state == 1)
-                                                @lang('form.denyManager')
-                                            @elseif($item->state == 2)
-                                                @lang('form.acceptManager')
-                                            @elseif($item->state == 3)
-                                                @lang('form.denyAdmin')
-                                            @elseif($item->state == 4)
-                                                @lang('form.acceptAdmin')
-                                            @elseif($item->state == 5)
-                                                @lang('form.denyLocation')
-                                            @elseif($item->state == 5)
-                                                @lang('form.acceptLocation')
-                                            @else
-                                                @lang('form.pending')
-                                            @endif
-                                        </td>
-                                        <td>{{$item->comments}}</td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
+                                    <tbody>
+                                    @foreach($data as $item)
+                                        <tr>
+                                            <td>{{$item->anno}}</td>
+                                            <td>{{$item->period}}</td>
+                                            <td>{{$item->course}}</td>
+                                            <td>{{$item->location}}</td>
+                                            <td>
+                                                @if($item->state == 1)
+                                                    @lang('form.denyManager')
+                                                @elseif($item->state == 2)
+                                                    @lang('form.acceptManager')
+                                                @elseif($item->state == 3)
+                                                    @lang('form.denyAdmin')
+                                                @elseif($item->state == 4)
+                                                    @lang('form.acceptAdmin')
+                                                @elseif($item->state == 5)
+                                                    @lang('form.denyLocation')
+                                                @elseif($item->state == 5)
+                                                    @lang('form.acceptLocation')
+                                                @else
+                                                    @lang('form.pending')
+                                                @endif
+                                            </td>
+                                            <td>{{$item->comments}}</td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
 
-                            </table>
-                        </div>
+                                </table>
+                            </div>
+                        @else
+                            <h2>@lang('form.badNews')</h2>
+                        @endif
+
                     </div>
                 </div>
             </div>
