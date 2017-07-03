74e7f7f225e5fb0e62e35a0259a9d24fc842e084
isaacrojasucr <jorgeisaac.rojas@ucrso.info>
2017-07-02 23:36:32 -0600

    aaa

--- database/migrations/2017_06_14_043247_create_pwcnm_inscription_requests_table.php
+++ database/migrations/2017_06_14_043247_create_pwcnm_inscription_requests_table.php
@@ -20,18 +20,19 @@ class CreatePwcnmInscriptionRequestsTable extends Migration
             $table->integer('phone');
             $table->string('email');
             $table->double('weightedAverage');
-            $table->integer('fk_course');
-            $table->foreign('fk_course')->references('id')->on('courses')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('fk_course')->unsigned();
             $table->integer('group');
             $table->integer('timesAttended');
             $table->string('observations',1000);
-            $table->integer('fk_process');
-            $table->foreign('fk_process')->references('id')->on('pwcnm_registration_processes')->onDelete('cascade')->onUpdate('cascade');
-            $table->integer('fk_career');
-            $table->foreign('fk_career')->references('id')->on('carrers')->onDelete('cascade')->onUpdate('cascade');
-            $table->integer('fk_location');
-            $table->foreign('fk_location')->references('id')->on('pwcnm_second_locations')->onDelete('cascade')->onUpdate('cascade');
+            $table->integer('fk_process')->unsigned();
+            $table->integer('fk_career')->unsigned();
+            $table->integer('fk_location')->unsigned();
             $table->timestamps();
+            
+            $table->foreign('fk_course')->references('id')->on('courses')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('fk_process')->references('id')->on('pwcnm_registration_processes')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('fk_career')->references('id')->on('carrers')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('fk_location')->references('id')->on('pwcnm_second_locations')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
